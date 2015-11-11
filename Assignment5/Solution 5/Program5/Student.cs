9e847ab5598304b9bf2f12804af98135ff175dc5
Jacob Douglass <jacob_douglass@att.net>
2015-11-10 19:50:21 -0500

    Outline

---
+++ Assignment5/Solution 5/Program5/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    class Student
+    {
+        public string Name;
+        public string SNumber;
+
+    }
+}
